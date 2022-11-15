1827c4d008f877f25db02002c791de8d89a1bad3
julienjego <jego.jj@gmail.com>
2022-11-15 09:41:57 +0100

    Typo

--- api/src/controllers/species.ts
+++ api/src/controllers/species.ts
@@ -14,7 +14,7 @@ const getAnimalsBySpecies = (
                     .then((animals) => res.status(200).json(animals))
                     .catch((error) => res.status(404).json({ error }));
             } else {
-                res.status(404).json({ message: "Animaux introuvable" });
+                res.status(404).json({ message: "Animaux introuvables" });
             }
         })
         .catch(() => {
