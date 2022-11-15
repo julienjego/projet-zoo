9764cece31a3e7cd0da2ca8ed955452fdfd783ea
julienjego <jego.jj@gmail.com>
2022-11-15 09:11:56 +0100

    Add get all animals by species request

--- api/src/controllers/species.ts
+++ api/src/controllers/species.ts
@@ -1,16 +1,19 @@
 import Species from "../models/species";
 import Animal from "../models/animal";
 import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import { ObjectId } from "mongodb";
 
 const getAnimalsBySpecies = (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    console.log(req.params.id);
-    const species = Species.findById(req.params.id).then((species) => {
+    Species.findById(req.params.id).then((species) => {
         if (species) {
-            console.log(species.nom);
+            Animal.find({ espece: species.nom })
+                .then((animals) => res.status(200).json(animals))
+                .catch((error) => res.status(404).json({ error }));
         } else {
             console.log("brr");
         }
