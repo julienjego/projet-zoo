45c17accab9a3e81730b36e2f37fd35572a5e32e
julienjego <jego.jj@gmail.com>
2022-11-16 12:04:06 +0100

    Add get all events for a zone

--- api/src/controllers/event.ts
+++ api/src/controllers/event.ts
@@ -2,8 +2,35 @@ import Event from "../models/event";
 import Enclosure from "../models/enclosure";
 import Species from "../models/species";
 import Animal from "../models/animal";
+import Zone from "../models/zone";
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
+
+//Récupère tous les événements d'une zone
+const getEventsByZone = (req: Request, res: Response, next: NextFunction) => {
+    Zone.findById(req.params.id)
+        .then((zone) => {
+            if (zone) {
+                Enclosure.find({ zone: zone.nom }).then((enclos) => {
+                    if (enclos) {
+                        const listEnclos = enclos.map((e) => ({
+                            enclos: e.nom,
+                        }));
+                        Event.find({ $or: listEnclos })
+                            .then((events) => res.status(200).json(events))
+                            .catch((error) => res.status(404).json({ error }));
+                    } else {
+                        res.status(404).json({
+                            message: "Aucune zone trouvée",
+                        });
+                    }
+                });
+            }
+        })
+        .catch(() => {
+            res.status(400).json({ erreur: "Syntaxe de la requête erronée" });
+        });
+};
 
 // Récupère tous les événements d'un enclos
 const getEventsByEnclosure = (
@@ -64,4 +91,9 @@ const getEventsByAnimal = (req: Request, res: Response, next: NextFunction) => {
         });
 };
 
-export default { getEventsByEnclosure, getEventsBySpecies, getEventsByAnimal };
+export default {
+    getEventsByEnclosure,
+    getEventsBySpecies,
+    getEventsByAnimal,
+    getEventsByZone,
+};
--- api/src/routes/event.ts
+++ api/src/routes/event.ts
@@ -3,8 +3,9 @@ import eventController from "../controllers/event";
 
 const router = express.Router();
 
-router.get("/enclosure/:id", eventController.getEventsByEnclosure);
+router.get("/enclosures/:id", eventController.getEventsByEnclosure);
 router.get("/species/:id", eventController.getEventsBySpecies);
 router.get("/animals/:id", eventController.getEventsByAnimal);
+router.get("/zones/:id", eventController.getEventsByZone);
 
 export default router;
