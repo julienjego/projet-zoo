d5936e29e023b8c0d071ea9d0d668cf42ed54ca2
julienjego <jego.jj@gmail.com>
2022-11-17 15:33:06 +0100

    Add some get routes for actions

--- api/src/controllers/action.ts
+++ api/src/controllers/action.ts
@@ -1,6 +1,8 @@
 import Action from "../models/action";
 import Zone from "../models/zone";
 import Enclosure from "../models/enclosure";
+import Species from "../models/species";
+import Animal from "../models/animal";
 import { NextFunction, Request, Response } from "express";
 
 // Création d'une action
@@ -47,4 +49,73 @@ const getActionsByZone = (req: Request, res: Response, next: NextFunction) => {
         });
 };
 
-export default { createAction, getActionsByZone };
+// Récupère toutes les actions d'un enclos
+const getActionsByEnclosure = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    Enclosure.findById(req.params.id)
+        .then((enclosure) => {
+            if (enclosure) {
+                Action.find({ enclos: enclosure.nom })
+                    .then((actions) => res.status(200).json(actions))
+                    .catch((error) => res.status(404).json({ error }));
+            } else {
+                res.status(404).json({ message: "Aucun enclos trouvé" });
+            }
+        })
+        .catch(() => {
+            res.status(400).json({ erreur: "Syntaxe de la requête erronée" });
+        });
+};
+
+// Récupère toutes les actions d'une espèce
+const getActionsBySpecies = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    Species.findById(req.params.id)
+        .then((species) => {
+            if (species) {
+                Action.find({ espece: species.nom })
+                    .then((actions) => res.status(200).json(actions))
+                    .catch((error) => res.status(404).json({ error }));
+            } else {
+                res.status(404).json({ message: "Aucune espèce trouvé" });
+            }
+        })
+        .catch(() => {
+            res.status(400).json({ erreur: "Syntaxe de la requête erronée" });
+        });
+};
+
+// Récupère toutes les actions d'un animal
+const getActionsByAnimal = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    Animal.findById(req.params.id)
+        .then((animal) => {
+            if (animal) {
+                Action.find({ animal: animal.nom })
+                    .then((actions) => res.status(200).json(actions))
+                    .catch((error) => res.status(404).json({ error }));
+            } else {
+                res.status(404).json({ message: "Aucun animal trouvé" });
+            }
+        })
+        .catch(() => {
+            res.status(400).json({ erreur: "Syntaxe de la requête erronée" });
+        });
+};
+
+export default {
+    createAction,
+    getActionsByZone,
+    getActionsByEnclosure,
+    getActionsBySpecies,
+    getActionsByAnimal,
+};
--- api/src/routes/action.ts
+++ api/src/routes/action.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.put("/create", actionController.createAction);
 router.get("/zones/:id", actionController.getActionsByZone);
+router.get("/enclosures/:id", actionController.getActionsByEnclosure);
+router.get("/species/:id", actionController.getActionsBySpecies);
+router.get("/animals/:id", actionController.getActionsByAnimal);
 
 export default router;
