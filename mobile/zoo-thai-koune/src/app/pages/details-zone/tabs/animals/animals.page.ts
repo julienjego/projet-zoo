49863d9787def2fae6dcbb54f375f32870335e84
julienjego <jego.jj@gmail.com>
2022-12-14 14:35:35 +0100

    launch animals tab

--- mobile/zoo-thai-koune/src/app/pages/details-zone/tabs/animals/animals.page.ts
+++ mobile/zoo-thai-koune/src/app/pages/details-zone/tabs/animals/animals.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from 'src/app/models/animal.model';
+import { AnimalService } from 'src/app/services/animal/animal.service';
+import { DetailsZonePage } from '../../details-zone.page';
 
 @Component({
   selector: 'app-animals',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animals.page.scss'],
 })
 export class AnimalsPage implements OnInit {
+  animals$: Observable<Animal[] | null> | undefined;
+  zoneId: string | null = this.detailsZonePage.getId();
 
-  constructor() { }
+  constructor(
+    private animalService: AnimalService,
+    private detailsZonePage: DetailsZonePage
+  ) {}
 
   ngOnInit() {
+    this.getAnimalsByZone();
   }
 
+  getAnimalsByZone() {
+    if (this.zoneId) {
+      this.animals$ = this.animalService.getAnimalsByZone(+this.zoneId);
+    }
+  }
 }
--- mobile/zoo-thai-koune/src/app/services/animal/animal.service.ts
+++ mobile/zoo-thai-koune/src/app/services/animal/animal.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Animal } from 'src/app/models/animal.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimalService {
+  API_URL = environment.API_URL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getAnimalsByZone(id: number): Observable<Animal[]> {
+    return this.http.get<Animal[]>(`${this.API_URL}/animals/zones/${id}`);
+  }
 }
