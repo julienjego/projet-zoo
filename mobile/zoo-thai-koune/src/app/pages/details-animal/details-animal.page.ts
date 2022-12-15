f76f85e151b5a09076999e1fc961630f426dea52
julienjego <jego.jj@gmail.com>
2022-12-15 11:18:33 +0100

    update details animal

--- mobile/zoo-thai-koune/src/app/pages/details-animal/details-animal.page.ts
+++ mobile/zoo-thai-koune/src/app/pages/details-animal/details-animal.page.ts
@@ -1,10 +1,9 @@
-import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AnimalService } from 'src/app/services/animal/animal.service';
 import { Animal } from 'src/app/models/animal.model';
-import { DetailsZonePage } from '../details-zone/details-zone.page';
-import { Location } from '@angular/common';
+import { Action } from 'src/app/models/action.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-details-animal',
@@ -14,11 +13,11 @@ import { Location } from '@angular/common';
 export class DetailsAnimalPage implements OnInit {
   animalId: string | null = this.route.snapshot.paramMap.get('id');
   animal: Animal | null | undefined;
+  actions$: Observable<Action[] | null> | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private animalService: AnimalService,
-    private location: Location
+    private animalService: AnimalService
   ) {}
 
   ngOnInit() {
@@ -26,7 +25,13 @@ export class DetailsAnimalPage implements OnInit {
       this.animalService
         .getAnAnimal(this.animalId)
         .subscribe((animal) => (this.animal = animal));
+
+      this.getActions(this.animalId);
     }
+  }
+
+  getActions(id: string) {
+    this.actions$ = this.animalService.getActionsOfAnAnimal(id);
   }
 
   goBack() {
