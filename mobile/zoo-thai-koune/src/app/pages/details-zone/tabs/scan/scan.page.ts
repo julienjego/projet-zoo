f16487e6ecdb15a2494d651448fb79f798beef59
julienjego <jego.jj@gmail.com>
2022-12-21 17:01:58 +0100

    qrcode scanning processing

--- mobile/zoo-thai-koune/src/app/pages/details-zone/tabs/scan/scan.page.ts
+++ mobile/zoo-thai-koune/src/app/pages/details-zone/tabs/scan/scan.page.ts
@@ -1,4 +1,7 @@
+import { Toasts } from 'src/app/utils/toasts';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Html5Qrcode } from 'html5-qrcode';
 
 @Component({
   selector: 'app-scan',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scan.page.scss'],
 })
 export class ScanPage implements OnInit {
+  html5QrcodeScanner!: Html5Qrcode;
 
-  constructor() { }
+  constructor(private router: Router, private toast: Toasts) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  startScan() {
+    this.html5QrcodeScanner = new Html5Qrcode('reader');
+    this.html5QrcodeScanner.start(
+      { facingMode: 'environment' },
+      { fps: 10, qrbox: { width: 300, height: 300 } },
+      this.onSuccess,
+      this.onFail
+    );
   }
 
+  onSuccess = (decodedText: string) => {
+    this.html5QrcodeScanner.stop();
+    this.router.navigate([decodedText]).catch(() => {
+      this.toast.presentToast('Aucun animal trouvé !');
+    });
+  };
+
+  onFail = () => {
+    console.warn('no match');
+  };
 }
