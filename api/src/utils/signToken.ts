0461d547fb0876a947c3cd1f92aeeec7035aec19
julienjego <jego.jj@gmail.com>
2022-12-21 10:31:17 +0100

    extend expire time

--- api/src/utils/signToken.ts
+++ api/src/utils/signToken.ts
@@ -13,7 +13,7 @@ const signToken = (
             {
                 issuer: config.token.issuer,
                 algorithm: "HS256",
-                expiresIn: 900,
+                expiresIn: 3600,
             },
             (error, token) => {
                 if (error) {
